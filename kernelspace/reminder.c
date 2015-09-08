03dec4ff2af094cf19ff3ac860fc4d19b4cb3235
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2015-09-08 12:26:06 +0200

    still does not work

--- kernelspace/reminder.c
+++ kernelspace/reminder.c
@@ -7,7 +7,7 @@
 
 MODULE_LICENSE("DUAL BSD/GPL");
 
-struct cdev reminder_cdev; //IT SHOULDN'T BE HERE
+struct cdev *reminder_cdev; //IT SHOULDN'T BE HERE??
 char *message;
 
 ssize_t reminder_read(struct file *f, char __user *buf, size_t nbytes, loff_t *ppos)
@@ -41,20 +41,41 @@ static int __init reminder_init(void)
 {
 	int rt;
 	dev_t dev;
+	size_t msg_size = 100;
 	rt = alloc_chrdev_region(&dev, 0, 1, "reminder");
 	if (rt)
 		return rt;
-	size_t msg_size = 100;
-	message = kmalloc(msg_size, GFP_KERNEL);
-	if (!message)
+	reminder_cdev = cdev_alloc();
+	if (!reminder_cdev) {
+		unregister_chrdev_region(dev, 1);
 		return -ENOMEM;
+	}
+	
+	message = kmalloc(msg_size, GFP_KERNEL);
+	if (!message) {
+		cdev_del(reminder_cdev); //SHOULD IT BE HERE?
+		unregister_chrdev_region(dev, 1);
+		return -ENOMEM;
+	}
 	memset(message, 0, msg_size);
+	cdev_init(reminder_cdev, &reminder_fops);
+	rt = cdev_add(reminder_cdev, dev, 1);
+	if (rt) {
+		cdev_del(reminder_cdev); //SHOULD IT BE HERE?
+		unregister_chrdev_region(dev, 1);
+		kfree(message);
+		return rt;
+	}
 	return 0;
 }
 
 static int __exit reminder_exit(void)
 {
+	int rt;
 	printk(KERN_EMERG "%s\n", message);
+	cdev_del(reminder_cdev);
+	unregister_chrdev_region(dev, 1);
+	kfree(message);	
 }
 
 module_init(reminder_init);
